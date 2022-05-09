7ef42d82063d0faac6eef0436e3d857fbaa1739a
tanglinxuework@163.com <tanglinxuework@163.com>
2022-05-09 22:45:44 +0800

    开发

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -6,7 +6,23 @@
 			<swiper-item><image src="https://t15.baidu.com/it/u=3254556675,1617290360&fm=55&app=54&fmt=auto?w=414&h=276" class="img"></image></swiper-item>
 		</swiper>
 		<button class="button" type="primary">开始报价</button>
-	
+		<uni-section title="卡片+列表" type="line" style='width:100%'>
+				<uni-card padding="0" spacing="0">
+					<template v-slot:cover>
+						<view class="custom-cover">
+							
+							<view class="cover-content">
+								<text class="uni-subtitle uni-white">今日新闻热点</text>
+							</view>
+						</view>
+					</template>
+					<uni-list>
+						<uni-list-item title="今日新闻" showArrow></uni-list-item>
+						<uni-list-item title="今日新闻" showArrow></uni-list-item>
+					</uni-list>
+					
+				</uni-card>
+			</uni-section>
 	</view>
 </template>
 
