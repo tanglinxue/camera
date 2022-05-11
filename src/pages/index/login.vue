733d422925da6b172cb9289ca17a71ab30ff3225
tanglinxue <31237473+tanglinxue@users.noreply.github.com>
2022-05-11 18:33:21 +0800

    开发

--- src/pages/index/login.vue
+++ src/pages/index/login.vue
@@ -1,24 +1,13 @@
 <template>
-	<view class="main column-center-a">
-		<view class='title'>登录</view>
-		<view class='column-center'>
-			<image src='/static/images/my/logo2.png' class='logo-img'></image>
-			<view class='logo-txt'>欢迎来到乐享到家</view>
+	<view class="main column-center">
+		<view class='column-center boxOuter logo'>
+			<image src='/static/images/logo.png' class='logo-img mgb20'></image>
+			<view class='txt mgb40'>影助理</view>
+			<view class='txt2'>活动会议影像从业者的小助理</view>
 		</view>
-		<view class='login-box mgb40'>
-			<image src='/static/images/my/phone.png' class='icon1'></image>
-			<input type="number" placeholder="请输入手机号" class='input' :inputBorder='false' v-model="mobile"
-				maxlength="11" />
-		</view>
-		<view class='login-box mgb100'>
-			<image src='/static/images/my/password.png' class='icon2'></image>
-			<input type="number" placeholder="请输入验证码" class='input' :inputBorder='false' v-model="captcha"
-				maxlength="6" />
-			<view class='code green' :class='{"disable":show}' @click='getCode'>
-				{{show?second+'后重新获取':'获取验证码'}}
-			</view>
-		</view>
-		<view class='main-btn' @click='login'>登录</view>
+		<view class="main-btn mgb40">微信授权</view>
+		
+		<view class='bottom-txt'>使用影助理，需要您授权微信呢称和头像</view>
 
 	</view>
 </template>
@@ -36,81 +25,6 @@
 				interval:null
 			}
 		},
-		methods: {
-			setClock() {
-				this.second = 60
-				this.interval = setInterval(() => {
-					this.second--;
-					if (this.second <= 0) {
-						this.show = false;
-						clearInterval(this.interval);
-					}
-				}, 1000);
-			},
-			async getCode() {
-				if (this.show) return
-				const mobile = this.mobile;
-				if (!/(^1[0-9]{10}$)/.test(mobile)) {
-					this.$methods.showToast('请输入正确的手机号码');
-					return;
-				}
-				try {
-					if (!this.canclick) return
-					this.canclick = false
-					this.$methods.showLoading(`发送中...`);
-					console.log('你好')
-					const res = await this.$API.home.getSms({
-						mobile
-					});
-					this.$methods.showToast(`获取验证码成功`);
-					this.show = true
-					this.setClock()
-				} catch (err) {
-					console.log(err)
-				} finally {
-					setTimeout(() => {
-						uni.hideLoading()
-						this.canclick = true
-					}, 600)
-				}
-			},
-			async login() {
-				const {
-					mobile,
-					captcha
-				} = this;
-				if (!/(^1[0-9]{10}$)/.test(mobile)) {
-					this.$methods.showToast('请输入正确的手机号码');
-					return;
-				}
-				if (!captcha) {
-					this.$methods.showToast('请先获取验证码');
-					return;
-				}
-
-				try {
-					if (!this.canclick) return
-					this.canclick = false
-					this.$methods.showLoading(`登录中...`);
-					const res = await this.$API.home.login({
-						mobile,
-						captcha
-					});
-					this.$methods.showToast(`登录成功`);
-					this.$store.dispatch('user/updateToken',res)
-					setTimeout(()=>{
-						 this.$jump("/pages/index/index", 3);
-					},1200)
-				} catch (err) {
-					console.log(err)
-				} finally {
-					setTimeout(() => {
-						uni.hideLoading()
-						this.canclick = true
-					}, 600)
-				}
-			}
-		}
 	}
 </script>
 
@@ -118,75 +32,28 @@
 	@import '@/static/scss/index.scss';
 
 	.main {
-		position: fixed;
-		width: 100%;
-		padding: 40rpx;
-		font-size: 28rpx;
-		height: 100%;
-		background: url(@/static/images/my/login.png);
-		background-size: 100% 100%;
-
-		.title {
-			font-size: 32rpx;
-			font-weight: bold;
-			text-align: center;
-			padding-top: 50rpx;
-			margin-bottom: 80rpx;
-		}
-
-		.logo-img {
-			width: 130rpx;
-			height: 130rpx;
-			margin-bottom: 20rpx;
-		}
-
-		.logo-txt {
-			font-size: 36rpx;
-			margin-bottom: 110rpx;
-		}
-
-		.login-box {
-			padding: 20rpx 30rpx;
-			border: 1px solid $border;
-			border-radius: 12rpx;
-			height: 100rpx;
-			display: flex;
-			align-items: center;
-			justify-content: center;
-
-			.icon1 {
-				width: 26rpx;
-				height: 36rpx;
-				margin-right: 28rpx;
-				position: relative;
-				top: -3rpx
+		padding:30rpx;
+		.logo{
+			padding:100rpx 0;
+			margin-bottom: 100rpx;
+			.logo-img{
+				width:150rpx;
+				height:150rpx;
 			}
-
-			.icon2 {
-				width: 34rpx;
-				height: 34rpx;
-				margin-right: 20rpx;
-				position: relative;
-				top: -3rpx
+			.txt{
+				font-size:36rpx;
 			}
-
-			.input {
-				flex: 1
-			}
-
-			.code {
-				padding-left: 30rpx;
-				height: 100%;
-				line-height: 60rpx;
-				border-left: 1px solid $maiBlack;
-				margin-left: 10rpx;
-
-				&.disable {
-					color: $gray !important;
-					border-color: $gray
-				}
+			.txt2{
+				font-size:28rpx;
+				color:$gray
 			}
 		}
-
+		.main-btn{
+			width:300rpx;
+		}
+		.bottom-txt{
+			color:$gray;
+			font-size:26rpx
+		}
 	}
 </style>
--- src/pages/my/info.vue
+++ src/pages/my/info.vue
@@ -1,100 +1,35 @@
 <template>
 	<view class="main column-center">
-		<view class='title'>
-			温馨提示：如需修改手机号码，生活照，个人简介等请与技师长联系修改
+		<view class='boxOuter'>
+			<view class="row borderbottom row-between">
+				<text class="tit">公司名称</text>
+				<input type="text" placeholder="请输入公司名称" class='input' :inputBorder='false' />
+			</view>
+			<view class="row borderbottom row-between">
+				<text class="tit">LOGO</text>
+				<view class='row-start'><image src='/static/images/logo.png' class='img'></image><uni-icons type="upload" size="26" class="icon"></uni-icons></view>
+			</view>
+			<view class="row borderbottom row-between">
+				<text class="tit">公司solgan</text>
+				<input type="text" placeholder="请输入公司solgan" class='input' :inputBorder='false' />
+			</view>
+			<view class="row borderbottom row-between">
+				<text class="tit">联系电话</text>
+				<input type="number" placeholder="请输入手机号" class='input' :inputBorder='false' v-model="mobile"
+					maxlength="11" />
+			</view>
+			<view class="row borderbottom row-between">
+				<text class="tit">微信二维码</text>
+				<view class='row-start'><text class='mgr15 gray'>请上传微信二维码</text><uni-icons type="upload" size="26" class="icon"></uni-icons></view>
+			</view>
+			<view class="row row-between">
+				<text class="tit">公众号二维码</text>
+				<view class='row-start'><text class='mgr15 gray'>公众号二维码</text><uni-icons type="upload" size="26" class="icon"></uni-icons></view>
+			</view>
+		
 		</view>
-		<view class='box'>
-			<view class="row-between person">
-				<view class="left row-start">
-					<view class="imgbox">
-						<image
-							:src="myInfo.face" mode="aspectFill">
-						</image>
-					</view>
-					<view class="column-center-a">
-						<view class="name">{{myInfo.nickname}}</view>
-						<view class="vip">技师ID：{{myInfo.id}}</view>
-					</view>
-				</view>
-			</view>
-			<view class="row borderbottom row-between">
-				<text class="tit">平台姓名</text>
-				<text class='val'>{{myInfo.nickname}}</text>
-			</view>
-			<view class="row borderbottom row-between">
-				<text class="tit">技师姓名</text>
-				<text class='val'>{{myInfo.realname}}</text>
-			</view>
-			<view class="row borderbottom row-between">
-				<text class="tit">账号状态</text>
-				<text class='val'>{{myInfo.status==0?'停牌':'正常'}}</text>
-			</view>
-			<view class="row borderbottom row-between">
-				<text class="tit">技师电话</text>
-				<text class='val'>{{myInfo.tel}}</text>
-			</view>
-			<view class="row borderbottom row-between">
-				<text class="tit">技师年龄</text>
-				<text class='val'>{{myInfo.age}}</text>
-			</view>
-			<view class="row borderbottom row-between">
-				<text class="tit">技师等级</text>
-				<text class='val'>{{gradeName}}</text>
-			</view>
-			<view class="row borderbottom" v-if='otherPics && otherPics.length'>
-				<view class="tit">个人简介</view>
-				<view class='tag-box'>
-					{{myInfo.introduce}}
-				</view>
-			</view>
-			
-			<view class="row borderbottom pb0">
-				<view class="tit">技师头像</view>
-				<view class='tag-box row-start wrap'>
-					<view class='img' v-for='(item,index) in [myInfo.face]' :key='index' @click='previewImage(item)'>
-						<image
-							:src="item" mode="aspectFill">
-						</image>
-					</view>
-				</view>
-			</view>
-			<view class="row borderbottom pb0" v-if='picArr && picArr.length'>
-				<view class="tit">技师生活照</view>
-				<view class='tag-box row-start wrap'>
-					<view class='img' v-for='(item,index) in picArr' :key='index' @click='previewImage(item)'>
-						<image
-							:src="item" mode="aspectFill">
-						</image>
-					</view>
-				</view>
-			</view>
-			<view class="row borderbottom pb0" v-if='myInfo.product_id && myInfo.product_id.length'>
-				<view class="tit">服务项目</view>
-				<view class='tag-box row-start wrap'>
-					<view class='tag' v-for='(item,index) in myInfo.product_id' :key='index'>{{item.title}}</view>
-				</view>
-			</view>
-			<view class="row borderbottom row-between">
-				<text class="tit">工作经验</text>
-				<text class='val'>{{myInfo.workyear}}年</text>
-			</view>
-			<view class="row borderbottom row-between">
-				<text class="tit">疫苗接种</text>
-				<text class='val'>{{myInfo.halthok==1?"已经接种":"暂未接种"}}</text>
-			</view>
-			<view class="row pb0" v-if='otherPics && otherPics.length'>
-				<view class="tit">其他证照</view>
-				<view class='tag-box row-start wrap'>
-					<view class='img' v-for='(item,index) in otherPics' :key='index'  @click='previewImage(item)'>
-						<image
-							:src="item" mode="aspectFill">
-						</image>
-					</view>
-				</view>
-			</view>
-			
-		</view>
-		<view class='main-btn' @click='back'>返回</view>
+		<view class='bottom-btn'><view class='main-btn' @click='back'>提交</view></view>
+		
 
 	</view>
 </template>
@@ -106,67 +41,7 @@
 				myInfo:{}
 			}
 		},
-		computed: {
-			picArr(){
-				if(this.myInfo.pic){
-					return this.myInfo.pic.split(',')
-				}else{
-					return []
-				}
-			},
-			otherPics(){
-				const {healthpic,jobpic,qualification} = this.myInfo
-				const arr = []
-				if(healthpic){
-					arr.push(healthpic)
-				}
-				if(jobpic){
-					arr.push(jobpic)
-				}
-				if(qualification){
-					arr.push(qualification)
-				}
-				return arr
-			},
-			gradeName(){
-				let grade = this.myInfo.grade;
-				switch(grade*1){
-					case 0:
-						return '初级'
-					case 1:
-						return '中级'	
-					case 2:
-						return '高级'
-					case 3:
-						return '特级'
-					case 4:
-						return '顾问'
-					default:
-						return ''
-				}
-			}
-		},
-		onLoad() {
-			this.getData();
-		},
-
-		methods: {
-			previewImage(url) {
-				uni.previewImage({
-					urls:[url]
-				});
-			},
-			async getData() {
-				this.$methods.showLoading();
-				this.myInfo = await this.$API.my.tech_detail();
-				uni.hideLoading()
-			},
-			back(){
-				uni.navigateBack({
-					delta:1
-				})
-			}
-		}
+		
 	}
 </script>
 
@@ -176,111 +51,34 @@
 	.main {
 		background: $backColor;
 		font-size: 28rpx;
-		padding-bottom: 20rpx;
 
-		.main-btn {
-			width: 710rpx;
-		}
-
-		.title {
-			color: #fff;
-			padding: 20rpx 30rpx;
-			line-height: 40rpx;
-			background: $greenColor;
-			margin-bottom: 20rpx;
-		}
-
-		.box {
-			width: 710rpx;
+		padding:30rpx;
+		.boxOuter {	
+			padding:0;
+			overflow: hidden;
 			background: #ffffff;
-			box-shadow: 0px 0px 8rpx 0px rgba(218, 218, 218, 0.5);
-			border-radius: 12rpx;
-			margin-bottom: 40rpx;
-			padding: 40rpx 30rpx 20rpx;
-
-			.person {
-				margin-bottom: 20rpx;
-
-				.left {
-					.imgbox {
-						width: 140rpx;
-						height: 140rpx;
-						border: 6rpx solid #F0F0F0;
-						border-radius: 100%;
-						margin-right: 40rpx;
-
-						image {
-							width: 100%;
-							height: 100%;
-							border-radius: 100%;
-						}
-					}
-
-					.name {
-						font-size: 36rpx;
-						font-weight: bold;
-						margin-bottom: 15rpx;
-					}
-
-					.vip {
-						font-size: 30rpx;
-						color: $gray
-					}
-				}
-			}
-
 			.row {
 				position: relative;
-				padding: 30rpx 0;
+				padding: 30rpx;
 				background: #fff;
 				font-size: 28rpx;
-				&.pb0{
-					padding-bottom: 0;
+				.input{
+					text-align: right;
 				}
-
-				.tag-box {
-					margin-top: 20rpx;
-
-					.tag {
-						width: 200rpx;
-						height: 72rpx;
-						line-height: 72rpx;
-						color: $gray;
-						text-align: center;
-						background: #F0F0F0;
-						border-radius: 12rpx;
-						margin-right: 20rpx;
-						margin-bottom: 20rpx;
-
-						&:nth-child(3n) {
-							margin-right: 0
-						}
-					}
-
-					.img {
-						width: 200rpx;
-						height: 200rpx;
-						border-radius: 12rpx;
-						margin-right: 20rpx;
-						margin-bottom: 20rpx;
-
-						image {
-							width: 100%;
-							height: 100%;
-						}
-
-						&:nth-child(3n) {
-							margin-right: 0
-						}
-					}
-				}
-
-				.val {
-					color: $gray
+				.img{
+					width:80rpx;
+					height:80rpx;
 				}
 			}
 		}
 
-
+		.bottom-btn {
+			width: 100%;
+			position: fixed;
+			padding: 20rpx;
+			bottom: 0;
+			left: 0;
+			background-color: $white;
+		}
 	}
 </style>
