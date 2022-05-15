81028a6913b3aee16d22db99a6ce884868a7d2b7
tanglinxuework@163.com <tanglinxuework@163.com>
2022-05-15 22:27:28 +0800

    开发

--- src/App.vue
+++ src/App.vue
@@ -38,4 +38,25 @@
 		font-size: 30rpx;
 		color: $mainBlack;
 	}
+	.uni-input {
+		.uni-numbox__minus.uni-cursor-point {
+			background-color: white;
+			border-radius: 12rpx 0 0 12rpx;
+			border: 1rpx solid #cccccc;
+		}
+		.uni-numbox {
+			.uni-numbox__value {
+				color: #232829;
+				font-size: 26rpx;
+			}
+			height: 60rpx;
+			line-height: 60rpx;
+		}
+		.uni-numbox__plus.uni-cursor-point {
+			background-color: white;
+			border-radius: 0 12rpx 12rpx 0;
+			border: 1rpx solid #cccccc;
+		}
+	}
+	
 </style>
