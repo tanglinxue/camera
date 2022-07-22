f77aeb8e12ae1b4f0b343a6326157d1539308301
tanglinxue <31237473+tanglinxue@users.noreply.github.com>
2022-07-22 13:26:58 +0800

    Revert "开发"
    This reverts commit e4f48989da312fc8e196f29eb27dbb41ed1715b3.

--- src/components/reduceCom.vue
+++ src/components/reduceCom.vue
@@ -10,7 +10,7 @@
 				</template>
 			</view>	
 			<view v-if="info.type == 'input' " class='input-box'>
-				<input  :value="info.num2" type='number' @input="inputTap" ref="input" />
+				<input  :value="info.num" type='number' @input="inputTap" ref="input" />
 			</view>	
 			<uni-number-box  :min="0" :max="99" v-model="info.num * 1" v-else-if='!info.noNum' @input='numTap' />
 		</view>
@@ -38,8 +38,7 @@ export default {
 	},
 	data(){
 		return {
-			showPopup:false,
-			num2:this.info.num
+			showPopup:false
 		}
 	},
 	methods: {
@@ -69,8 +68,10 @@ export default {
 			if(parentId){
 				params.parentId = parentId;
 			}
-			this.num2 = val
-			this.$store.commit('service/changeServiceObj',params)
+			console.log()
+			this.$nextTick(()=>{
+				this.$store.commit('service/changeServiceObj',params)
+			})	
 		},
 		numTap(num){
 			const {id,parentId} = this.info;
